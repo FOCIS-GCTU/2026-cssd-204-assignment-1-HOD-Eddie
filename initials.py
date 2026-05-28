c7dd2a5dcc4c0339184589472995e11b209d2507
Edmund <edmundaduve@gmail.com>
2026-05-28 15:33:24 +0000

    initials.py ammended

--- initials.py
+++ initials.py
@@ -1,7 +1,35 @@
+# File: initials.py
+# Description: A Python program that prints the initials of my name (Edmund Aduve)
+#             where I chose "N" as a stand in initial, using only the print() method and strings of my initials
+# Assignment Number: Assignment Number, 2
+#
+# Name: Edmund Aduve
+# Student ID: 2425400319
+# Email: 2425400319@live.gctu.edu.gh
+# Grader: Augustus
+#
+# On my honor, Edmund Aduve, this programming assignment is my on work
+# and I have not provided this code to any other student.
+
 
 def main():
-  pass #code goes here
+    # my main function defined on line 15, contains my assignment source code and this comment.
 
+    print()
+    print("...EAN")
+    print()
+    print("...EEEEEEEEEEE............AAAA............NN........NN.....")
+    print("...EEEEEEEEEEE...........AAAAAA...........NNN.......NN.....")
+    print("...EE...................AA....AA..........NNNN......NN.....")
+    print("...EE..................AA......AA.........NNNNN.....NN.....")
+    print("...EEEEEEEEE..........AAAAAAAAAAAA........NN..NN....NN.....")
+    print("...EEEEEEEEE..........AAAAAAAAAAAA........NN....NN..NN.....")
+    print("...EE.................AA........AA........NN.....NNNNN.....")
+    print("...EE.................AA........AA........NN......NNNN.....")
+    print("...EEEEEEEEEEE...**...AA........AA...**...NN.......NNN...**")
+    print("...EEEEEEEEEEE...**...AA........AA...**...NN........NN...**")
+    print()
 
 
 main()
+
