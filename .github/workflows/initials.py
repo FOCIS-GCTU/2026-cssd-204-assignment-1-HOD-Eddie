22198585e144b2e3c7abd26f56e0d11547e10791
Edmund <edmundaduve@gmail.com>
2026-05-28 15:07:21 +0000

    Add initials.py, Assigment Number, 2

---
+++ .github/workflows/initials.py
@@ -0,0 +1,28 @@
+# File: initials.py
+# Description: A Python program that prints the initials of my name (Edmund Aduve)
+#             where I chose "N" as a stand in initial, using only the print() method and strings of my initials
+# Assignment Number: Assignment Number, 2
+#
+# Name: Edmund Aduve
+# Student ID: 2425400319
+# Email: 2425400319@live.gctu.edu.gh
+# Grader: Augustus
+#
+# On my honor, Edmund Aduve, this programming assignment is my on work
+# and I have not provided this code to any other student.
+
+def main():
+    print()
+    print("...EAN")
+    print()
+    print("...EEEEEEEEEEE............AAAA............NN........NN.....")
+    print("...EEEEEEEEEEE...........AAAAAA...........NNN.......NN.....")
+    print("...EE...................AA....AA..........NNNN......NN.....")
+    print("...EE..................AA......AA.........NNNNN.....NN.....")
+    print("...EEEEEEEEE..........AAAAAAAAAAAA........NN..NN....NN.....")
+    print("...EEEEEEEEE..........AAAAAAAAAAAA........NN....NN..NN.....")
+    print("...EE.................AA........AA........NN.....NNNNN.....")
+    print("...EE.................AA........AA........NN......NNNN.....")
+    print("...EEEEEEEEEEE...**...AA........AA...**...NN.......NNN...**")
+    print("...EEEEEEEEEEE...**...AA........AA...**...NN........NN...**")
+    print()
